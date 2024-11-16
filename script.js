4162e3d32377ae6f3e37779d81b0c8e7e46017cb
mayurhedaoo100 <mayurhedaoo100@gmail.com>
2024-11-16 11:26:21 +0530

    Launch portfolio

--- script.js
+++
@@ -1,5 +0,0 @@
-// JavaScript for adding dynamic interactivity (if needed)
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio site loaded');
-    // Add interactivity as needed
-});
