608960bc2d66e0a5cbc1ff0bf5f20aec35e08186
mayurhedaoo100 <mayurhedaoo100@gmail.com>
2024-11-19 01:34:30 +0530

    "Added import statements for CONTACT constant and framer-motion library, updated h2 element to use motion from framer-motion, and added contact information section with address and email"

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,5 @@
+import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 const Contact = () => {
@@ -33,7 +35,10 @@ const Contact = () => {
 
   return (
     <div className="border-b border-neutral-900 pb-20">
-      <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
+      <motion.h2
+        initial={{ y: -100, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5 }} className="my-10 text-center text-4xl">Get in Touch</motion.h2>
 
       <form
         name="contact"
@@ -80,6 +85,13 @@ const Contact = () => {
 
       {formStatus === 'success' && <p className="text-green-500 text-center">Form submitted successfully!</p>}
       {formStatus === 'error' && <p className="text-red-500 text-center">There was an error with the submission. Please try again.</p>}
+
+      <div className="text-center tracking-tighter">
+        <p className="my-4">{CONTACT.address}</p>
+        <a href="mailto:mayurh100@gmail.com" className="border-b">
+          {CONTACT.email}
+        </a>
+      </div>
     </div>
   );
 };
