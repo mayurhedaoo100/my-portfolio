bdebb24fefcbd804962f1757e29ec7107c516a49
mayurhedaoo100 <mayurhedaoo100@gmail.com>
2024-11-18 11:13:45 +0530

    contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,6 +10,42 @@ const Contact = () =>{
             transition={{duration: 0.5}}
             className="my-10 text-center text-4xl">Get in Touch</motion.h2>
 
+<form name="contact" netlify class="max-w-lg mx-auto p-6">
+    <input
+      type="text"
+      id="name"
+      name="name"
+      placeholder="Your Name"
+      required
+      class="w-full font-thin mb-3 px-4 py-2 border bg-transparent border-neutral-700 rounded-md focus:outline-none focus:ring-2 focus:ring-neutral-700"
+    />
+
+<input
+      type="tel"
+      id="phone"
+      name="phone"
+      placeholder="Phone number"
+      class="w-full font-thin mb-3 px-4 py-2 border bg-transparent border-neutral-700 rounded-md focus:outline-none focus:ring-2 focus:ring-neutral-700"
+    />
+  
+    <input
+      type="email"
+      id="email"
+      name="email"
+      placeholder="Email"
+      required
+      class="w-full font-thin mb-4 px-4 py-2 border bg-transparent border-neutral-700 rounded-md focus:outline-none focus:ring-2 focus:ring-neutral-700"
+    />
+
+  <button
+    type="submit"
+    class="w-full py-2 bg-black text-white font-thin rounded-md hover:bg-neutral-900 transition duration-200"
+  >
+    Send
+  </button>
+</form>
+
+
             <div className="text-center tracking-tighter">
                 <p className="my-4">{CONTACT.address}</p>
                 <a href="mailto:mayurh100@gmail.com" className="border-b">{CONTACT.email}</a>
