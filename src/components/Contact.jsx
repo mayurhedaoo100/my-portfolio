65901dccae63268bc49e428b46c22f86e63e9668
mayurhedaoo100 <mayurhedaoo100@gmail.com>
2024-11-18 22:16:40 +0530

    "Simplified contact form layout and styling"

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,41 +10,16 @@ const Contact = () =>{
             transition={{duration: 0.5}}
             className="my-10 text-center text-4xl">Get in Touch</motion.h2>
 
-<form name="contact" method="POST" netlify action="/thank-you"
- class="max-w-lg mx-auto p-6">
-<input type="hidden" name="form-name" value="contact" />
-    <input
-      type="text"
-      id="name"
-      name="name"
-      placeholder="Your Name"
-      required
-      class="w-full font-thin mb-3 px-4 py-2 border bg-transparent border-neutral-700 rounded-md focus:outline-none focus:ring-2 focus:ring-neutral-700"
-    />
-
-<input
-      type="tel"
-      id="phone"
-      name="phone"
-      placeholder="Phone number"
-      class="w-full font-thin mb-3 px-4 py-2 border bg-transparent border-neutral-700 rounded-md focus:outline-none focus:ring-2 focus:ring-neutral-700"
-    />
-  
-    <input
-      type="email"
-      id="email"
-      name="email"
-      placeholder="Email"
-      required
-      class="w-full font-thin mb-4 px-4 py-2 border bg-transparent border-neutral-700 rounded-md focus:outline-none focus:ring-2 focus:ring-neutral-700"
-    />
-
-  <button
-    type="submit"
-    class="w-full py-2 bg-black text-white font-thin rounded-md hover:bg-neutral-900 transition duration-200"
-  >
-    Send
-  </button>
+<form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
 </form>
 
 
